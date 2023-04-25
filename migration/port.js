20ad1b9408f63664534ce98b8a520d93d9beb15f
Peak Nattapat <peak@finnomena.com>
2023-04-25 22:27:54 +0700

    edit port: bulkwrite

--- migration/port.js
+++ migration/port.js
@@ -13,11 +13,12 @@ export const port = (mongo_db, mongo_collection, mongo_uri, path) => {
       const records = data.split('\n').map((line) => line.split(','))
       const recordsWithoutHeader = records.slice(1)
       let updatedDocuments = 0
+      let updateArray = []
 
       for (let row of recordsWithoutHeader) {
         let user_id = parseInt(row[0]) || 0
         let doc = {
-          agent_account_id: row[1] || '',
+          agent_account_id: row[1] !== 'NULL' ? row[1] : null,
           account_type: row[2] || '',
           plan_slot: parseInt(row[3]) || 0,
           plan_id: parseInt(row[4]) || 0,
@@ -31,12 +32,31 @@ export const port = (mongo_db, mongo_collection, mongo_uri, path) => {
           is_hide: parseInt(row[13]) || 0
         }
         let updated_at = new Date()
+        updateArray.push({
+          updateOne: {
+            filter: {
+              user_id: user_id,
+              'port.plan_id': { $ne: doc['plan_id'] }
+            },
+            update: {
+              $set: { updated_at: updated_at },
+              $addToSet: { port: doc }
+            }
+          }
+        })
+      }
 
+      let chunkSize = 20000
+      let totalRows = updateArray.length
+      let n = Math.Ceil(totalRows / chunkSize)
+      for (let i = 0; i < n; i++) {
+        let start = i * chunkSize
+        let end = (i + 1) * chunkSize
+        if (end > totalRows) {
+          end = totalRows
+        }
         await collection
-          .updateOne(
-            { user_id: user_id, 'port.plan_id': { $ne: doc['plan_id'] } },
-            { $set: { updated_at: updated_at }, $addToSet: { port: doc } }
-          )
+          .bulkWrite(updateArray.slice(start, end))
           .then((res) => {
             updatedDocuments += res.matchedCount
             console.log(`Updated ${updatedDocuments} records`)

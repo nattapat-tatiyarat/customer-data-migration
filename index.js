9d32e6496206cbc6c5c2201cbf954ceb7a22d874
Peak Nattapat <peak@finnomena.com>
2023-04-26 00:22:26 +0700

    create external-id cmd

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ import * as migration from './migration/index.js'
 const argv = minimist(process.argv.slice(2))
 const { migrate, db, collection, uri, path } = argv
 const migrations = {
-	'auth-email': migration.email,
-	port: migration.port,
-	identity: migration.identity,
-	accounts: migration.accounts
+  'auth-email': migration.email,
+  port: migration.port,
+  identity: migration.identity,
+  accounts: migration.accounts,
+  'external-id': migration.externalID
 }
 const migrateFn = migrations[migrate]
 
 if (!migrateFn) {
-	console.error(`
+  console.error(`
 	Available value of 'migrate': ${Object.keys(migrations)}
   `)
 } else if (!migrate || !db || !collection || !uri || !path) {
-	console.error(`
+  console.error(`
     Missing or Invalid argument(s):
     node index.js --migrate <MIGRATE> --db <MONGO_DB> --collection <MONGO_COLLECTION> --uri <MONGO_URI> --path <PATH_TO_CSV>
   `)
 } else {
-	migrateFn(db, collection, uri, path)
+  migrateFn(db, collection, uri, path)
 }
---
+++ migration/external-id.js
@@ -0,0 +1,58 @@
+import { MongoClient } from 'mongodb'
+import { readFile } from 'fs'
+
+export const externalID = (mongo_db, mongo_collection, mongo_uri, path) => {
+  MongoClient.connect(mongo_uri, { useNewUrlParser: true }, (err, client) => {
+    if (err) throw err
+    console.log('Connected successfully to server')
+
+    const collection = client.db(mongo_db).collection(mongo_collection)
+
+    readFile(path, 'utf8', async (err, data) => {
+      if (err) throw err
+      const records = data.split('\n').map((line) => line.split(','))
+      const recordsWithoutHeader = records.slice(1)
+      let updatedDocuments = 0
+      let updateArray = []
+
+      for (let row of recordsWithoutHeader) {
+        let user_id = parseInt(row[0]) || 0
+        let doc = {
+          external_id: row[1] || ''
+        }
+        updateArray.push({
+          updateOne: {
+            filter: {
+              user_id: user_id
+            },
+            update: {
+              $set: doc
+            }
+          }
+        })
+      }
+
+      let chunkSize = 20000
+      let totalRows = updateArray.length
+      let n = Math.ceil(totalRows / chunkSize)
+      for (let i = 0; i < n; i++) {
+        let start = i * chunkSize
+        let end = (i + 1) * chunkSize
+        if (end > totalRows) {
+          end = totalRows
+        }
+        await collection
+          .bulkWrite(updateArray.slice(start, end))
+          .then((res) => {
+            updatedDocuments += res.matchedCount
+            console.log(`Updated ${updatedDocuments} records`)
+          })
+          .catch((err) => {
+            console.error(err)
+          })
+      }
+
+      client.close()
+    })
+  })
+}
